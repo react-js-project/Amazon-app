cba905d1dcf6f1c2edcbc6b72e22b4e74393b2ff
vaibhavkumar1 <vaibhav.kumar_cs18@gla.ac.in>
2020-09-12 21:07:23 +0530

    Updates

---
+++ precache-manifest.cacbe1531c4d70d8898a134a13cd7845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a211b3e7ca2b47131b3708bce1ec91",
+    "url": "/Amazon-app/index.html"
+  },
+  {
+    "revision": "a69b13e89d9c06fc52c3",
+    "url": "/Amazon-app/static/css/main.026906af.chunk.css"
+  },
+  {
+    "revision": "977ac1e998450d66c2df",
+    "url": "/Amazon-app/static/js/2.8f207bdf.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Amazon-app/static/js/2.8f207bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69b13e89d9c06fc52c3",
+    "url": "/Amazon-app/static/js/main.3c500d1e.chunk.js"
+  },
+  {
+    "revision": "33f20897461eaae8f6ef",
+    "url": "/Amazon-app/static/js/runtime-main.f48b9a74.js"
+  }
+]);
